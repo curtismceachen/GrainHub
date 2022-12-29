1f883677073267e6d7419b66e022b67fee17ed81
user <curtis.w.mceachen@gmail.com>
2022-12-29 13:59:22 -0500

    MERN shell

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+    <div className="App">
+      Investing Ideas
+    </div>
+    )
+  }
+}
+
