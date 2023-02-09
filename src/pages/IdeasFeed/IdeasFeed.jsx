94be275630de26820b2c2457cfd0287394dc8031
user <curtis.w.mceachen@gmail.com>
2023-02-09 14:34:40 -0500

    ideasFeed styling

--- src/pages/IdeasFeed/IdeasFeed.jsx
+++ src/pages/IdeasFeed/IdeasFeed.jsx
@@ -4,6 +4,7 @@ import DOMPurify from 'dompurify'
 import {useParams} from 'react-router'
 import './IdeasFeed.css'
 import bullish from '../../images/bullish.svg'
+import bearish from '../../images/bearish.png'
 
 
 export default function IdeasFeed (props) {
@@ -44,46 +45,28 @@ export default function IdeasFeed (props) {
             <div className="col-md-3"></div>
             <div className="col-md-6">
             {ideasWithPubUsername.map((i) =>
-            <div className="ideaCard border">
-              {/* <div className="card card-spacing ideaCard"> */}
-              <div className="panel-body panelPadding">
-                <section className="post-heading">
-                  <div className="row">
-                    <div className="col-md-2">
-                      {/* <div className=""> */}
-                        <div className="media-left">
-                          <img className='media-object photo-profile' src={i.publisher.profilePic}></img>
-                        </div>
+              <div className="ideaCard border">
+                <div className="ideaPanelMargin">
+                  <section className="post-heading">
+                    <div className="ideaPanelProfilePic">
+                      <img className='profilePic' src={i.publisher.profilePic}></img>
                     </div>
-                    <div className='col-md-10'>
-                      <div className='media-body'>
-                        <h6 className=""><Link to={`/publishers/show/${i.publisher.id}`} style={{ color: 'inherit', textDecoration: 'inherit' }}>{i.publisher.username}</Link></h6>
-                      </div>
-                    </div>
-                  </div>
-                </section>
-                {/* <section className='post-body'> */}
-                <section className="">
-                  <h5 className="ideaPanelTitle">{i.title}</h5>
-                  {/* <div className=''> */}
-                    {/* <div className='col-md-6'> */}
-                    {i.ticker && <div className="ideaPanelTicker ideaPanelSubheading">{i.ticker}
-                    <svg xmlns="http://www.w3.org/2000/svg" className='' width="16" height="16" fill="currentColor" class="bi bi-dot" viewBox="0 0 16 16">
-                      <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
-                    </svg>
+                    <h6 className='ideaPanelUsername'><Link to={`/publishers/show/${i.publisher.id}`} style={{ color: 'inherit', textDecoration: 'inherit' }}>{i.publisher.username}</Link></h6>
+                  </section>
+                  <section className="">
+                    <h5 className="ideaPanelTitle">{i.title}</h5>
+                    {i.ticker && <div className="ideaPanelSubheading">{i.ticker}
+                      <svg xmlns="http://www.w3.org/2000/svg" className='' width="16" height="16" fill="currentColor" class="bi bi-dot" viewBox="0 0 16 16">
+                        <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+                      </svg>
                     </div>}
-                    {/* </div> */}
-                    {/* <div className='col-md-6'> */}
                     {i.longOrShort && <div className="ideaPanelSubheading">{i.longOrShort}</div>}
-                    {i.longOrShort === 'Long' && <img className='bullishImg' src={bullish}></img>}
-                    {/* </div> */}
-                  {/* </div> */}
-                  <div className="" dangerouslySetInnerHTML={sanitizedData(i.thesis)}></div>
-                
-                </section>
-                {/* </section> */}
+                    {i.longOrShort === 'Long' && <img className='longOrShortImg' src={bullish}></img>}
+                    {i.longOrShort === 'Short' && <img className='longOrShortImg' src={bearish}></img>}
+                    <div dangerouslySetInnerHTML={sanitizedData(i.thesis)}></div>
+                  </section>
+                </div>
               </div>
-            </div>
             )}
             </div>
             <div className="col-md-3"></div>
